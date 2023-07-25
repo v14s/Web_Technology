c2f3da7f457765ebdd308c0d2cd474446c2ed211
v14s <92673214+v14s@users.noreply.github.com>
2023-07-25 23:57:36 +0530

    Create index1.php

---
+++ HTML_Validation_Webinar_Registration/index1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <h1 style="color: red">Registration for :Webinar Registration Information</h1>
+        <label>Company: <input type="text" name="company" /></label><br />
+        <label>First Name: <input type="text" name="first_name" /></label><br />
+        <label>Last Name: <input type="text" name="last_name" /></label><br />
+        <label>Email: <input type="text" name="email" /></label><br />
+        <label>Phone: <input type="text" name="phone" /></label><br />
+        <label>Address: <input type="text" name="address" /></label><br />
+        <label>City: <input type="text" name="city" /></label><br />
+        <label>State: <input type="text" name="state" /></label><br />
+        <input type="submit" value="Submit My Registration" />
+      </form>
+</body>
+</html>
